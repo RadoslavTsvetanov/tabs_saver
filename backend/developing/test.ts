c371745f3d1ef4080ba77adef414268bc2245abe
RadoslavCvetanov <radoslav.a.tsvetanov.2021@elsys-bg.org>
2024-03-07 17:33:54 +0200

    implemented all the CRUD functionality for the base functioning of the fromtned. I think?

--- backend/developing/test.ts
+++ backend/developing/test.ts
@@ -1,13 +1,40 @@
 import {DB} from "../src/db_repo"
-
-
-
+ enum changes{
+    CLOSED_TAB = "closedTab",
+    CREATED_TAB = "createdTab",
+    CHANGED_TAB = "changedTab",
+    TAKEN_NEW_SNAPSHOT = "newSnapshot"
+}
 
 let db = new DB()
 
 
+let user
+// db.addSession(1, {
+//   creation_date: new Date(), BaseSnapshotData: {
+//   tabs: [{tab_id_given_from_chrome_api:1,url:"hihi",title:"haha"}]
+//   }
+// }).then((data) => {
+//   user = data;
+// })
 
-db.addSession(7, {
-  creation_date: new Date(), BaseSnapshotData: {
-  tabs: [{tab_id_given_from_chrome_api:1,url:"hihi",title:"haha"}]
-}})
+
+// db.getUser("hhuhuhuu").then((data) => {
+//   console.log(data)
+// // })
+
+// if (user == null) {
+//   user = {
+//     id:1
+//   }
+// }
+
+
+
+db.addChange(3,{type_of_change: changes.CHANGED_TAB,tab:{tab_id_given_from_chrome_api:1,title:"hoho",url:"jiji"}})
+
+
+
+db.getSession(3).then((session) => {
+  console.log(session)
+})
--- backend/src/db_repo.ts
+++ backend/src/db_repo.ts
@@ -1,4 +1,4 @@
-import { PrismaClient, Snapshot } from '@prisma/client';
+import { Change, PrismaClient, Snapshot } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
@@ -48,6 +48,87 @@ export class DB {
             throw error;
         }
     }
+
+async addChange(sessionId: number, change: { type_of_change: string, tab: { tab_id_given_from_chrome_api: number, url: string, title: string }}) {
+  try {
+    // Create the session if it doesn't exist
+    let session = await prisma.session.findFirst({
+      where: {
+        id: sessionId,
+      }
+    });
+
+    if (!session) {
+      return
+    }
+    const tab = await prisma.tab.create({
+      data: {
+          tab_id_given_from_chrome_api: change.tab.tab_id_given_from_chrome_api,
+          url: change.tab.url,
+          title: change.tab.title
+        }
+      });
+
+    // Create the change
+    const newChange = await prisma.change.create({
+      data: {
+        type_of_change: change.type_of_change,
+        tabId: tab.id,
+        sessionId: session.id
+      }
+    });
+
+    return newChange;
+  } catch (error) {
+    console.error("Error adding change:", error);
+    throw error;
+  }
+}
+
+  
+/*
+
+ the sessionId is Global so every sessionId is unique to the overall sessions table and not relative
+
+*/
+  async getSession(sessionId: number) {
+    const session = prisma.session.findFirst({
+      where: {
+        id:sessionId
+      },
+      include: {
+        baseSnapshot: true,
+        changes: true
+      }
+    })
+
+    return session;
+  }
+
+
+  async getUser(username: string) {
+    try {
+        const user = await prisma.user.findUnique({
+            where: {
+                name: username
+            },
+            include: {
+                sessions: {
+                    include: {
+                        baseSnapshot: true,
+                        changes: true,
+                    }
+                },
+            }
+        });
+
+        return user;
+    } catch (e) {
+        console.error(e);
+        throw e;
+    }
+}
+
 }
 
 
