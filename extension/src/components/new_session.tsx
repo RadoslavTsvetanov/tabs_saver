583f22448adf9f5489bd199c8b0b3543b7739db9
RadoslavCvetanov <radoslav.a.tsvetanov.2021@elsys-bg.org>
2024-03-10 02:36:49 +0200

    did some UX things

--- extension/src/components/new_session.tsx
+++ extension/src/components/new_session.tsx
@@ -5,33 +5,46 @@ import { api } from "../utils/api";
 import { Tab } from "../models/Users";
 import { UserContext } from "../utils/constext";
 export const NewSession: React.FC<{ userId: number }> = ({ userId }) => {
-  const [is_form_toggled] = useState(false);
+  const [is_warning_showed, set_is_warnng_showed] = useState(false);
   const user = useContext(UserContext);
+  console.log(is_warning_showed);
   return (
     <div className="mt-8">
       <Button
         text={"Start new browser session"}
-        on_click={async () => {
-          const tabs = await TabManager.logAllTabs();
-          console.log("user id -> ");
-          const new_session = await api.addSessionToUser(userId, {
-            creation_date: new Date(),
-            baseSnapshot: {
-              tabs: tabs as Tab[],
-            },
-
-            // these bottom two are obsolete since backend created them by itself but its to satisfy the type definition
-            id: 0,
-            changes: [],
-          });
-          console.log("new session -> ", new_session);
-          await user.setCurrentSession(new_session.id);
+        on_click={() => {
+          console.log("clicking button");
+          set_is_warnng_showed(true);
         }}
       />
-      {is_form_toggled ?? (
-        <>
-          <form></form>
-        </>
+      {is_warning_showed && (
+        <div className="absolute top-1 right-1 bg-slate-600">
+          <h1>
+            Are you sure you want to create a new session, if you have more than
+            5 the last one will be deleted automatically if you dont have a
+            premium acc
+          </h1>
+          <Button
+            on_click={async () => {
+              const tabs = await TabManager.logAllTabs();
+              console.log("user id -> ");
+              const new_session = await api.addSessionToUser(userId, {
+                creation_date: new Date(),
+                baseSnapshot: {
+                  tabs: tabs as Tab[],
+                },
+
+                // these bottom two are obsolete since backend created them by itself but its to satisfy the type definition
+                id: 0,
+                changes: [],
+              });
+              console.log("new session -> ", new_session);
+              await user.setCurrentSession(new_session.id);
+              set_is_warnng_showed(false);
+            }}
+            text="create new session"
+          />
+        </div>
       )}
     </div>
   );
