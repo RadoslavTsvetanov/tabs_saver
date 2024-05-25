d85303fac4e47341b162e5a0abee10df2c21271f
RadoslavCvetanov <radoslav.a.tsvetanov.2021@elsys-bg.org>
2024-05-25 21:58:31 +0300

    more deployment config fixes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -68,7 +68,8 @@ app.get("/user/:username", async (req: Request, res: Response) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
+export default app;
